ebec7a7891441193c5ff4ab1d602d1dd106eab0d
AlenaBelenets <alenaharenkova@gmail.com>
2023-05-05 14:14:43 +0300

    Added turns of players

---
+++ TicTacToeGame/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  TicTacToeGame
+//
+//  Created by Alena Belenets on 05.05.2023.
+//
+
+import Foundation
+
+struct Gamers {
+    var nameOfFirstPlayer = "First Player"
+    var nameOfSecondPlayer = "Second Player"
+}
+
+struct Game {
+    let zero = "O"
+    let cross = "X"
+}
+
--- TicTacToeGame/TicTacToeGameViewController.swift
+++ TicTacToeGame/TicTacToeGameViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 enum Turn {
-    case Null
+    case Zero
     case Crosse
 }
 
 class TicTacToeGameViewController: UIViewController {
 
-
+    let ticTacToe = Gamers()
+    let turn = Game()
 
 
     @IBOutlet weak var gamesTurnLabel: UILabel!
@@ -32,15 +33,34 @@ class TicTacToeGameViewController: UIViewController {
     var firstTurn = Turn.Crosse
     var currentTurn = Turn.Crosse
 
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
 
     }
 
 
     @IBAction func tapOnScreenAction(_ sender: UIButton) {
+        addToGame(sender)
     }
 
+    func addToGame(_ sender: UIButton) {
+        guard sender.title(for: .normal) == nil else { return }
+
+        if currentTurn == Turn.Zero {
+            sender.setTitle(turn.zero, for: .normal)
+            currentTurn = Turn.Crosse
+            gamesTurnLabel.text = ticTacToe.nameOfFirstPlayer
+        }
+        else if currentTurn == Turn.Crosse {
+            sender.setTitle(turn.cross, for: .normal)
+            currentTurn = Turn.Zero
+            gamesTurnLabel.text = ticTacToe.nameOfSecondPlayer
+        }
+
+    }
 
 }
 
