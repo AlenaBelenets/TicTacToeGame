5391cfaaf7d2f9bbf5f2d675ca11e4de5a1be865
AlenaBelenets <alenaharenkova@gmail.com>
2023-05-06 14:04:14 +0300

    Added AddNamesViewController + storyboard

---
+++ TicTacToeGame/AddNamesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddNamesViewController.swift
+//  TicTacToeGame
+//
+//  Created by Alena Belenets on 06.05.2023.
+//
+
+import UIKit
+
+class AddNamesViewController: UIViewController {
+
+    @IBOutlet weak var firstPlayerLabel: UITextField!
+
+    @IBOutlet weak var secondPlayerLabel: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
